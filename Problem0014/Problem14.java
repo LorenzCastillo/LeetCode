9b4db7d0fa68362751b476bc1edcc4d2ffba18a6
Lorenz Castillo <lorenz5524@hotmail.com>
2023-01-09 23:50:10 -0800

    14. Longest Common Prefix

---
+++ Problem0014/Problem14.java
@@ -0,0 +1,13 @@
+package Problem0014;
+
+public class Problem14 {
+    public static void main(String[] args) {
+        String[] arr = { "flower", "flow", "flight" };
+
+        System.out.println(longestCommonPrefix(arr));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        return "Hello";
+    }
+}
